af58a569c24f4d1ce426358ceaeae02deb7b46f2
Segun Osiki <osiki.segun@gmail.com>
2023-01-29 09:32:31 +0100

    add fields to review class

--- src/main/java/com/osiki/moviesapi/Review.java
+++ src/main/java/com/osiki/moviesapi/Review.java
@@ -1,4 +1,19 @@
 package com.osiki.moviesapi;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = "review")
 public class Review {
+    @Id
+    private ObjectId id;
+    private String body;
 }
