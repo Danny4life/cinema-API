6e7f682f1084c252248bff5930dc013d3e2bf8eb
Segun Osiki <osiki.segun@gmail.com>
2023-01-29 09:28:07 +0100

    add fields to movie class

--- src/main/java/com/osiki/moviesapi/Movie.java
+++ src/main/java/com/osiki/moviesapi/Movie.java
@@ -1,14 +1,22 @@
 package com.osiki.moviesapi;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.List;
 
 
+@Data
+@AllArgsConstructor //takes care of constructor method with private fields
+@NoArgsConstructor // this takes no parameter
 @Document(collection = "movies")
 public class Movie {
 
+    @Id
     private ObjectId id;
     private String imdbId;
     private String title;
