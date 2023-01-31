0f4e5c72d8838084d5a5cb58d5e999e0a285ec67
Segun Osiki <osiki.segun@gmail.com>
2023-01-31 14:59:05 +0100

    use imdbid as movie id

--- src/main/java/com/osiki/moviesapi/MovieRepository.java
+++ src/main/java/com/osiki/moviesapi/MovieRepository.java
@@ -4,6 +4,12 @@ import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface MovieRepository extends MongoRepository<Movie, ObjectId> {
+
+    // to find movie by id using imdbId instead of objectId so as not expose our user id to the public
+
+    Optional<Movie> findMovieByImbdbId(String imdbId);
 }
